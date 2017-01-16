a3a0a31f1f5531c7a229a9bbb5e074d707539bdc
Ian Fare <apellius@gmail.com>
2017-01-15 22:32:00 -0500

    Commented final copy

--- runge_kutta.py
+++ runge_kutta.py
@@ -3,72 +3,119 @@
 import matplotlib.pyplot as plt
 import math
 
+###########################################################
+###########################################################
+
+# ~~~~~~~~ System of ODEs information goes here ~~~~~~~~~~~
+
+
+# Define any number of functions
+# i.e. the RHS of ODEs
 def f1(x,y1,y2):
   return y2
 
 def f2(x,y1,y2):
-  b = 0.5
-  c = 1.0
-  return -b*y2 - c*c*y1
+  b = 1.0
+  c = 5.0
+  return -b*y2 - c**2*y1
 
+# Add them to the functions list
 functions = [f1,f2]
 
-# Initial conditions, at x_0
+# Define initial conditions, at the lower bound of x
 A = 1.0
-b = 0.5
-c = 1.0
+b = 1.0
+c = 5.0
 ics = [A,-A*b/2]
 
-# Values of x to evaluate
-h = 0.000001
-x_lim = [0.0,20.0]
+# Specify domain to run, and h
+h = 0.001
+x_lim = [0.0,7.0]
+
+###########################################################
+###########################################################
+
+
+# ~~~~~~~~~~~~~~~ Runge-Kutta starts here ~~~~~~~~~~~~~~~~~
+
+# Here, the 0th index in the functions list, the k lists,
+# and the y list refers to information relating to y_0
+# (or y_1 if you're starting with y_1), and so forth
 
 # Skipping the first subroutine to find derivatives
 # because it's just one line, calling the functions...
 
-# Second subroutine to call derivatives, calculate ks and tys
+# Second subroutine to call derivatives, calculate ks and ys
 # Arguments should be:
 #   0  : x
 #   1+ : y_i
 def find_ys(argList):
 
   y_results = []
+
+  k1 = []
+  # Calculate k_1,i
+  # For each function (from the DEs),
   for func_i,func in enumerate(functions):
     k1_args = []
-    # x value is unchanged
+    # Add the previous x value to the function's arguments
     k1_args.append(argList[0])
+    # Add all the y values to the function's arguments
     for i,element in enumerate(argList):
       if i > 0:
         k1_args.append(element)
-    k1 = h * func(*k1_args)
+    # Call the functions, and record k values for each
+    k1.append(h * func(*k1_args))
 
+  k2 = []
+  # Calculate k_2,i
+  # For each function (from the DEs),
+  for func_i,func in enumerate(functions):
     k2_args = []
+    # Add the previous x value + h/2 to the function's arguments
     k2_args.append(argList[0] + h/2)
+    # Add all the y values + (k1,i)/2 to the function's arguments
     for i,element in enumerate(argList):
       if i > 0:
-        k2_args.append(element + k1/2)
-    k2 = h * func(*k2_args)
+        k2_args.append(element + k1[i-1]/2)
+    # Call the functions, and record k values for each
+    k2.append(h * func(*k2_args))
 
+  k3 = []
+  # Calculate k_3,i, as above
+  for func_i,func in enumerate(functions):
     k3_args = []
     k3_args.append(argList[0] + h/2)
     for i,element in enumerate(argList):
       if i > 0:
-        k3_args.append(element + k2/2)
-    k3 = h * func(*k2_args)
+        k3_args.append(element + k2[i-1]/2)
+    k3.append(h * func(*k2_args))
 
+  k4 = []
+  # Calculate k_4,i
+  for func_i,func in enumerate(functions):
     k4_args = []
     k4_args.append(argList[0] + h)
     for i,element in enumerate(argList):
       if i > 0:
-        k4_args.append(element + k3)
-    k4 = h * func(*k4_args)
+        k4_args.append(element + k3[i-1])
+    k4.append(h * func(*k4_args))
 
-    y_results.append(argList[func_i+1] + k1/6 + k2/3 + k3/3 + k4/6)
+  # Calculate the new y_i values 
+  for func_i,func in enumerate(functions):
+    y_results.append(argList[func_i+1] + k1[func_i]/6 + k2[func_i]/3 + k3[func_i]/3 + k4[func_i]/6)
   
+  # Return the newly calculated y values
   return y_results
 
 analytic = []
 
+# Main routine:
+#   1) Prepares x values to evaluate
+#   2) Prepares lists as elements of list for y_i values
+#   3) Puts initial conditions into those lists
+#   4) Runs find_ys() for each x value sequentially
+#   5) Plots analytic solution (can remove this if there is none)
 def main_routine(functions,ics,h,x_lim):
   # Prepare xs to evaluate
   xs = []
@@ -97,15 +144,16 @@ def main_routine(functions,ics,h,x_lim):
       for i,element in enumerate(ys):
         element.append(new_ys[i])
 
-  # Plot analytic solution
+  # Compute analytic solution
   for i in xs:
     analytic.append(A*math.exp(-b*i/2)*math.cos(c*i))
 
+  # Plot stuff
   plt.plot(xs,ys[0],label="Runge-Kutta solution")
   plt.plot(xs,analytic,label="Analytic solution")
   plt.xlabel("x")
   plt.ylabel("y_1")
-  plt.title("Runge-Kutta and Analytic Solutions from x="+str(x_lim[0])+" to x="+str(x_lim[1])+" with h="+str(h))
+  plt.title("Runge-Kutta and Analytic Solutions from x="+str(x_lim[0])+" to x="+str(x_lim[1])+"\nwith b=" +str(b)+",c="+str(c)+",h="+str(h))
   plt.legend()
   plt.show()
 
