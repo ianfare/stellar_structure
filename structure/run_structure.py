6e20448e02cf90450103179a819db84fe6c203bd
ianfare <apellius@gmail.com>
2017-03-29 19:28:42 -0400

    Deleting obsolete files, starting report

--- structure/run_structure.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import subprocess
-
-for i in range(20000000):
-  print str(0.711+float(i)/10000000000)
-  subprocess.Popen("~/school/stellar_structure/structure/structure.py " + str(0.711+float(i)/10000000000),shell=True).wait()
